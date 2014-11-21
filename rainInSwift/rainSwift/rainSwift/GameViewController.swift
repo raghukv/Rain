02076cca5475900e80b79e7d51e94b19f29704ef
Raghu Kalyan <raghukv11188@gmail.com>
2014-11-21 01:08:34 -0500

    remove spaces

--- rainInSwift/rainSwift/rainSwift/GameViewController.swift
+++ rainInSwift/rainSwift/rainSwift/GameViewController.swift
@@ -11,46 +11,35 @@ import SpriteKit
 
 extension SKNode {
     class func unarchiveFromFile(file : NSString) -> SKNode? {
-        
         let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks")
-        
         var sceneData = NSData(contentsOfFile: path!, options: .DataReadingMappedIfSafe, error: nil)
         var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData!)
-        
         archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
         let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
         archiver.finishDecoding()
         scene.backgroundColor = SKColor.whiteColor()
-        
         return scene
     }
 }
-
+    
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         var sceneWidth = self.view.bounds.width * 4
         var sceneHeight = self.view.bounds.height * 4
         var size = CGSizeMake(sceneWidth, sceneHeight)
         var gameScene = GameScene(size: size)
             gameScene.backgroundColor = SKColor.whiteColor()
-            
-            
-            
             let skView = self.view as SKView
             skView.showsDrawCount = true
             skView.showsFPS = true
             skView.showsNodeCount = true
-        
-            
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-        
             skView.presentScene(gameScene)
-
-}
+    }
+    
     override func loadView() {
         self.view = SKView(frame: UIScreen.mainScreen().bounds)
     }
